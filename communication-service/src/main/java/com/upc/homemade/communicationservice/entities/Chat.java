26096287b7750f60305dd924d8d007d729285177
kimjongappreciator <nonexistentexistentialist1@gmail.com>
2022-05-01 13:49:24 -0500

    chat class

---
+++ communication-service/src/main/java/com/upc/homemade/communicationservice/entities/Chat.java
@@ -0,0 +1,19 @@
+package com.upc.homemade.communicationservice.entities;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table(name = "chats")
+@Data
+public class Chat {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "dateOfChat")
+    private Date date;
+
+}
