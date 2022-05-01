e8112f1717ad725f2b05925938a445f9b7a57f6b
RickCBs <erikc.cortezb@gmail.com>
2022-05-01 14:07:21 -0500

    services

---
+++ communication-service/src/main/java/com/upc/homemade/communicationservice/services/PublicationService.java
@@ -0,0 +1,7 @@
+package com.upc.homemade.communicationservice.services;
+
+import com.upc.homemade.communicationservice.entities.Publication;
+
+public interface PublicationService extends CrudService<Publication, Long>{
+
+}
