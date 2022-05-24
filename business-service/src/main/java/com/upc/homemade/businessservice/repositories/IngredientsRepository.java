309523244b138fb0a4537c5ef53d3045602e988a
kimjongappreciator <nonexistentexistentialist1@gmail.com>
2022-05-23 23:58:39 -0500

    config modified

--- business-service/src/main/java/com/upc/homemade/businessservice/repositories/IngredientsRepository.java
+++ business-service/src/main/java/com/upc/homemade/businessservice/repositories/IngredientsRepository.java
@@ -1,5 +1,6 @@
 package com.upc.homemade.businessservice.repositories;
 
+
 import com.upc.homemade.businessservice.entities.Ingredients;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
